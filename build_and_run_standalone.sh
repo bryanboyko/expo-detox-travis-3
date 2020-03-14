8d2bf589a1d000f3f7d03d8d8f85021a94092c3d
Bryan Boyko <bryan@dopplerlabs.com>
2020-03-13 21:00:45 -0700

    Open expo build directly instead of via Exponent.app

--- build_and_run_standalone.sh
+++ build_and_run_standalone.sh
@@ -1,7 +1,7 @@
 expo build:ios --type simulator --release-channel e2e_test
 expo build:status
 wget https://exp-shell-app-assets.s3.us-west-1.amazonaws.com/ios/%40wen-kai/expo-detox-travis-3-c95eacf9-c334-4c48-9379-42c6214e028e-simulator.tar.gz
-tar -xvzf https://exp-shell-app-assets.s3.us-west-1.amazonaws.com/ios/%40wen-kai/expo-detox-travis-3-c95eacf9-c334-4c48-9379-42c6214e028e-simulator.tar.gz
+tar -xvzf expo-detox-travis-3-c95eacf9-c334-4c48-9379-42c6214e028e-simulator.tar.gz
 open -a `xcode-select -p`/Applications/Simulator.app
 xcrun simctl install booted expo-detox-travis-3.app
 xcrun simctl launch booted com.expoDetoxTravis.expoDetoxTravis
